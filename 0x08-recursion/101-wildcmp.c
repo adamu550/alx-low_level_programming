85780bcad3bb242e5fd24035c2331880e7e70c6b
adam <aadamu7@yahoo.com>
2022-09-28 23:08:00 +0100

    recursion

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,26 +1,22 @@
 #include "main.h"
 /**
- * wildcmp - checkif two words are the same
- * @s1: string 1
- * @s2: string 2
- * Return: 0
+ * wildcmp - compare function
+ * @s1: Character pointer
+ * @s2: Character pointer
+ * Return: 0, 1 or wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2)
  */
 int wildcmp(char *s1, char *s2)
 {
-	if (*s1 == *s2 && *s1 != '\0' && *s2 != '\0')
-	{
-		return (wildcmp(s1 + 1, s2 + 1));
-	}
+	if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
+		return (0);
+
 	if (*s1 == '\0' && *s2 == '\0')
-	{
 		return (1);
-	}
+
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+
 	if (*s2 == '*')
-	{
-		if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
-		{
-			return (1);
-		}
-	}
+		return ((wildcmp(s1, s2 + 1)) || (wildcmp(s1 + 1, s2)));
 	return (0);
 }
