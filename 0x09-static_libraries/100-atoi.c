3bfbb49d8e493e4dde3d388947dd92ac0c5809de
adam <aadamu7@yahoo.com>
2022-09-30 15:51:28 -0700

    library tasks

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,1 +1,9 @@
+#include "main.h"
+
+/**
+ * _atoi - function description
+ *@s: pointer to
+ * Return: always 0.
+ */
+
 int _atoi(char *s);
--- 0x09-static_libraries/9-strcpy.c
+++ 0x09-static_libraries/9-strcpy.c
@@ -1,1 +1,11 @@
+#include "main.h"
+
+/**
+ * _strycpy - function description
+ *
+ * @dest: pointer to
+ * @src: pointer to
+ * Return: char
+ */
+
 char *_strcpy(char *dest, char *src);
