68ae02870cf869b50a220b6a3cd0d6c9e3a58cc2
adam <aadamu7@yahoo.com>
2022-10-11 23:12:20 +0100

    structures

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - define a new dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: dogs' owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
