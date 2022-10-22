09c3e8735689f5243c8089f1c6964fd707035337
adam <aadamu7@yahoo.com>
2022-10-22 19:41:00 +0100

    lower aplha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints alphabets in lower case
+ *
+ * Return: 0 success.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
+
+
