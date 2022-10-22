9f7395dba90ae919b853c79a60e8fd9cc95642de
adam <aadamu7@yahoo.com>
2022-10-22 22:55:49 +0100

    digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints alphabets in lower case
+ *
+ * Return: 0 success.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
+
